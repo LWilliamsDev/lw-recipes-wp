fb7703c20def64326b79369f04e5f42ec1d1a7ab
Lisa Williams <lwilliams@admins-MacBook-Pro.local>
2026-06-14 07:51:36 -0400

    Vite setup for theme

---
+++ wp-content/themes/lw-recipes/src/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    outDir: resolve(__dirname, '../assets'),
+    // 1. Enables minification (Vite uses esbuild by default, which is incredibly fast)
+    minify: 'esbuild', 
+    rollupOptions: {
+
+      input: {
+        // 2. Define your entry points so Vite knows what to compile
+        style: resolve(__dirname, './assets/css/style.css'), // Your main HTML file
+        global: resolve(__dirname, './assets/js/global-js.js'), // Replace with your specific JS file path
+      },
+      output: {
+        // Keeps files named cleanly instead of adding a hash (optional)
+        entryFileNames: 'js/[name].js',
+        assetFileNames: 'css/[name].[ext]',
+      },
+    },
+  },
+});
