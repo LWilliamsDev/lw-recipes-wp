61e4341d49e95711be9ad04a2e04f81d8b737f41
Lisa Williams <lwilliams@MacBookPro.lan>
2026-07-19 13:53:11 -0400

    Fix bug where related recipes displayed the title in the editor if there were no results, fix layout issue with the recipe overview block

--- wp-content/mu-plugins/recipes/blocks/build/recipe-overview/index.asset.php
+++ wp-content/mu-plugins/recipes/blocks/build/recipe-overview/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'd93e19f1717f8c4a2644');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '881b7c8a339c5271a353');
--- wp-content/mu-plugins/recipes/blocks/build/recipe-related/index.asset.php
+++ wp-content/mu-plugins/recipes/blocks/build/recipe-related/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'c2b3cbec296bbc49001e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'e60d12694d70c33b142f');
