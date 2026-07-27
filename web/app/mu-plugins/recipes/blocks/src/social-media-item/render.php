685e8bd6c6df36156b199c45c65b2eea8ed3e37b
Lisa Williams <lwilliams@MacBookPro.lan>
2026-07-27 15:12:24 -0400

    Convert social media item block to Twig

--- web/app/mu-plugins/recipes/blocks/src/social-media-item/render.php
+++ web/app/mu-plugins/recipes/blocks/src/social-media-item/render.php
@@ -3,6 +3,7 @@
 namespace Recipes\Blocks\SocialMediaItem;
 
 use Recipes\Blocks\BlockHelper;
+use Timber\Timber;
 
 $is_block_editor = BlockHelper::is_in_block_editor();
 
@@ -26,10 +27,24 @@ if (!$is_block_editor && !defined('REST_REQUEST') && !(wp_doing_ajax())) {
 		)
 	]; 
 
-	$icon = $attributes['item']['icon'];
-	?>
-	
-	<li><a href="<?php echo $attributes['item']['site']; ?>" target="_blank"><img src="<?php echo $social_media[$icon]['img']; ?>" alt="<?php echo $social_media[$icon]['alt'];?>"></a>
+	$link = $attributes['item']['site'];
+	$icon_attr = $attributes['item']['icon'];
+	$icon = null;
+	$alt = null;
 
-<?php } ?>
+	if (!empty($social_media[$icon_attr])) {
+		$icon = $social_media[$icon_attr]['img'];
+		$alt = $social_media[$icon_attr]['alt'];
+	}
+
+		Timber::render('social-media-item/view.twig',
+    [
+        'link' => $link,
+        'icon' => $icon,
+        'alt' => $alt
+    ]
+);
+
+
+ } ?>
 
